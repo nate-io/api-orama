29694835641b9705a68d6232263a5cc8ceca3ac7
Nathaniel Young <nyoung125@gmail.com>
2020-05-18 07:52:37 -0400

    add roles table migration and user fk

--- pg-node/server/db/models/user.js
+++ pg-node/server/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     isActive: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
-    // associations can be defined here
+    User.hasOne(models.Role)
   };
   return User;
 };
